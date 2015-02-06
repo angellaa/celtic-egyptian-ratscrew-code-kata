aeb6448b74a2fc201a0f3091f69625741cb83aef
Andrea Angella <angella.andrea@gmail.com>
2015-02-06 17:08:07 +0000

    Add minimal game test
    We spent a lot of time thinking about the minimal meaningful game. This
    helped us to think about the API and understand the rules of the game
    better.

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/GameTests.cs
@@ -0,0 +1,45 @@
+﻿using CelticEgyptianRatscrewKata.SnapRules;
+using Moq;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    class GameTests
+    {
+        [Test]
+        public void NewGame()
+        {
+            var dealer = new Dealer();
+            
+            var randomGenerator = new Mock<IRandomNumberGenerator>();
+            randomGenerator.Setup(x => x.Get(0, It.IsAny<int>())).Returns(0);
+
+            var shuffler = new Shuffler(randomGenerator.Object);
+            var deck = Cards.With(
+                new Card(Suit.Clubs, Rank.Three),
+                new Card(Suit.Diamonds, Rank.Three)
+                );
+
+            var rule = new Mock<IRule>();
+            rule.Setup(x => x.CanSnap(It.IsAny<Cards>())).Returns(true);
+
+            var game = new Game(dealer, shuffler, new[] {rule.Object}, deck);
+
+            var player1 = new Player();
+            var player2 = new Player();
+            player1.Join(game);
+            player2.Join(game);
+
+            game.Start();
+
+            player1.LayCard();
+            player2.LayCard();
+            player2.CallSnap();
+
+            Assert.AreEqual(player2, game.Winner());
+            Assert.IsEmpty(player1.Pile);
+            Assert.AreEqual(deck, player2.Pile);
+            Assert.IsEmpty(game.Stack);
+        }
+    }
+}
